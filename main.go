89e74c9912ac48249f76e7f28d1997989faad283
Melanie Rush <merush@microsoft.com>
2019-08-01 16:17:06 -0600

    matt to the rescue

--- main.go
+++ main.go
@@ -15,12 +15,17 @@ func main() {
 	ctx := context.Background()
 
 	//create new server
-	// _, err := sql.CreateServer(ctx, "meltestserver", "merush", "test123456!!")
+	if _, err := sql.CreateServer(ctx, "meltestserver", "merush", "test123456!!"); err != nil {
+		fmt.Println("error is: ", err)
+	}
+
 	//create new DB
-	_, err := sql.CreateDB(ctx, "meltestserver", "meltestdb")
-	//delete a DB
-	// _, err := sql.DeleteDB(ctx, "meltestserver", "meltestdb")
-	if err != nil {
+	if _, err := sql.CreateDB(ctx, "meltestserver", "meltestdb"); err != nil {
+		fmt.Println("error is: ", err)
+	}
+
+	//delete DB
+	if _, err := sql.DeleteDB(ctx, "meltestserver", "meltestdb"); err != nil {
 		fmt.Println("error is: ", err)
 	}
 }
