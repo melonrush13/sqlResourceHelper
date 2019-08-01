7675974f49694a00e19b6e96e387a70f39862052
Melanie Rush <merush@microsoft.com>
2019-08-01 16:18:09 -0600

    commenting out unneeded calls

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ func main() {
 	config.LoadSettings()
 	ctx := context.Background()
 
-	//create new server
-	if _, err := sql.CreateServer(ctx, "meltestserver", "merush", "test123456!!"); err != nil {
-		fmt.Println("error is: ", err)
-	}
+	// //create new server
+	// if _, err := sql.CreateServer(ctx, "meltestserver", "merush", "test123456!!"); err != nil {
+	// 	fmt.Println("error is: ", err)
+	// }
 
 	//create new DB
 	if _, err := sql.CreateDB(ctx, "meltestserver", "meltestdb"); err != nil {
 		fmt.Println("error is: ", err)
 	}
 
-	//delete DB
-	if _, err := sql.DeleteDB(ctx, "meltestserver", "meltestdb"); err != nil {
-		fmt.Println("error is: ", err)
-	}
+	// //delete DB
+	// if _, err := sql.DeleteDB(ctx, "meltestserver", "meltestdb"); err != nil {
+	// 	fmt.Println("error is: ", err)
+	// }
 }
