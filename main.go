e3037d18b30c9a06e54948ffa6e98884302ffe02
Melanie Rush <merush@microsoft.com>
2019-08-01 16:13:44 -0600

    fixing spaces because erin said so hehe

--- main.go
+++ main.go
@@ -16,13 +16,10 @@ func main() {
 
 	//create new server
 	// _, err := sql.CreateServer(ctx, "meltestserver", "merush", "test123456!!")
-
 	//create new DB
 	_, err := sql.CreateDB(ctx, "meltestserver", "meltestdb")
-
 	//delete a DB
 	// _, err := sql.DeleteDB(ctx, "meltestserver", "meltestdb")
-
 	if err != nil {
 		fmt.Println("error is: ", err)
 	}
