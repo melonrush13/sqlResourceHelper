b823ad15147931cfd6f992efa4632816349f34ee
Melanie Rush <merush@microsoft.com>
2019-08-01 16:10:33 -0600

    adding functions for create new DB and delete DB

--- main.go
+++ main.go
@@ -15,7 +15,13 @@ func main() {
 	ctx := context.Background()
 
 	//create new server
-	_, err := sql.CreateServer(ctx, "melRushServer2019", "merush", "test123456!!")
+	// _, err := sql.CreateServer(ctx, "meltestserver", "merush", "test123456!!")
+
+	//create new DB
+	_, err := sql.CreateDB(ctx, "meltestserver", "meltestdb")
+
+	//delete a DB
+	// _, err := sql.DeleteDB(ctx, "meltestserver", "meltestdb")
 
 	if err != nil {
 		fmt.Println("error is: ", err)
